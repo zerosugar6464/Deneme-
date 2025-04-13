ab5b5692058bdd83e276ebd7536c8841f8277633
zerosugar6464 <musickumal@gmail.com>
2025-04-13 20:21:01 +0300

    Update Rosetagger.py

--- Rosetagger.py
+++ Rosetagger.py
@@ -93,11 +93,11 @@ except:
   os.system("pip install deep-translator --break-system-packages")
 os.system('cls' if os.name == 'nt' else 'clear') 
 
-OWNER_ID = owner id gir 
-API_ID = api id gir 
-API_HASH = api hash gir 
-BOT_TOKEN = bot token gir
-client = TelegramClient('LionUserBot', API_ID, API_HASH)
+OWNER_ID = 7525624006 
+API_ID = 20824282
+API_HASH = 5c49d99b5bb9e41c9b8ada4f826989ef 
+BOT_TOKEN = 7336757348:AAGW8yt7dNW50odjiUDNhrKtQ32zSMbC2tM
+client = TelegramClient('Silataggerbot', API_ID, API_HASH)
 bot = AsyncTeleBot(BOT_TOKEN)
 
 SUBSCRIPTIONS_FILE = "subscriptions.json"
